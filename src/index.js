f989b0f9dd3b4a8173c8cc6452a36456ccb3a9cc
Kartikey <kartikeysharma1181@gmail.com>
2026-08-20 02:20:43 +0530

    Some more progress

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// import mongoose from "mongoose"; 
+// import { DB_NAME } from "./constants.js";
+// import express from "express";
+// const app=express();   
+// (async()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+//         app.on("error",(error)=>{
+//             console.error("ERROR:",error)
+//             throw error;
+//         })
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+
+//     }
+//     catch(error){
+//         console.error("ERROR:",error)
+//         throw error
+//     }
+
+
+
+// })()
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+    path:"./env"
+});
+
+connectDB()

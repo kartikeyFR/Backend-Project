049b638d44cb1874a244d92569022be2756c0a3b
Kartikey <kartikeysharma1181@gmail.com>
2026-08-30 01:19:49 +0530

    nothing much

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,7 @@
-import {asyncHandler} from "../utils.asyncHandler.js"
-import {ApiError} from "../utils.ApiError.js"
-import {User} from "../models.user.model.js"
-import {uploadOnCloudinary} from "../utils.cloudinary.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 
 const registerUser=asyncHandler(async (req,res)=>{
@@ -29,7 +29,14 @@ const registerUser=asyncHandler(async (req,res)=>{
     }
 
     const avatarLocalPath=req.files?.avatar[0].path
-    const coverImageLocalPath=req.files?.coverImage[0].path
+    // const coverImageLocalPath=req.files?.coverImage[0].path
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
+
+
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar is required")
